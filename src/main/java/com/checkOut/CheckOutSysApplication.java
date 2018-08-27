822543753fe49bfb4f968a76811cfbba993759c3
yuanfanqi <yuan_fanqi@163.com>
2018-08-27 11:02:24 +0800

    修改

--- src/main/java/com/checkOut/CheckOutSysApplication.java
+++ src/main/java/com/checkOut/CheckOutSysApplication.java
@@ -2,8 +2,12 @@ package com.checkOut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
+@ComponentScan(basePackages="com.checkOut")
 public class CheckOutSysApplication {
 
 	public static void main(String[] args) {
